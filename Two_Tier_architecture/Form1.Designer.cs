e8da4d4f62693866748695d4d9826187ea5bdb48
dana <m.photocam9@gmail.com>
2022-05-15 01:20:14 +0300

    Add project files.

---
+++ Two_Tier_architecture/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace Two_Tier_architecture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtnamesrch = new System.Windows.Forms.TextBox();
+            this.srchbtn = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txttax = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtsalery = new System.Windows.Forms.TextBox();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnview = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label8.Location = new System.Drawing.Point(513, 444);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 17);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "Search By Name";
+            // 
+            // txtnamesrch
+            // 
+            this.txtnamesrch.Location = new System.Drawing.Point(501, 465);
+            this.txtnamesrch.Margin = new System.Windows.Forms.Padding(4);
+            this.txtnamesrch.Name = "txtnamesrch";
+            this.txtnamesrch.Size = new System.Drawing.Size(144, 22);
+            this.txtnamesrch.TabIndex = 22;
+            // 
+            // srchbtn
+            // 
+            this.srchbtn.Location = new System.Drawing.Point(688, 461);
+            this.srchbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.srchbtn.Name = "srchbtn";
+            this.srchbtn.Size = new System.Drawing.Size(100, 28);
+            this.srchbtn.TabIndex = 23;
+            this.srchbtn.Text = "Search";
+            this.srchbtn.UseVisualStyleBackColor = true;
+            this.srchbtn.Click += new System.EventHandler(this.srchbtn_Click_1);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Location = new System.Drawing.Point(109, 455);
+            this.btnnew.Margin = new System.Windows.Forms.Padding(4);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(100, 28);
+            this.btnnew.TabIndex = 21;
+            this.btnnew.Text = "NEW";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtage);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txttax);
+            this.panel1.Controls.Add(this.txtid);
+            this.panel1.Controls.Add(this.txtname);
+            this.panel1.Controls.Add(this.txtsalery);
+            this.panel1.Location = new System.Drawing.Point(33, 42);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 295);
+            this.panel1.TabIndex = 25;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(129, 210);
+            this.txtage.Margin = new System.Windows.Forms.Padding(4);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(132, 22);
+            this.txtage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 164);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 210);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "age";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "male",
+            "female"});
+            this.comboBox1.Location = new System.Drawing.Point(129, 110);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1, 256);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "tax";
+            // 
+            // txttax
+            // 
+            this.txttax.Location = new System.Drawing.Point(129, 260);
+            this.txttax.Margin = new System.Windows.Forms.Padding(4);
+            this.txttax.Name = "txttax";
+            this.txttax.Size = new System.Drawing.Size(132, 22);
+            this.txttax.TabIndex = 5;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(129, 12);
+            this.txtid.Margin = new System.Windows.Forms.Padding(4);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(132, 22);
+            this.txtid.TabIndex = 0;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(129, 65);
+            this.txtname.Margin = new System.Windows.Forms.Padding(4);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(132, 22);
+            this.txtname.TabIndex = 1;
+            // 
+            // txtsalery
+            // 
+            this.txtsalery.Location = new System.Drawing.Point(129, 164);
+            this.txtsalery.Margin = new System.Windows.Forms.Padding(4);
+            this.txtsalery.Name = "txtsalery";
+            this.txtsalery.Size = new System.Drawing.Size(132, 22);
+            this.txtsalery.TabIndex = 3;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(157, 401);
+            this.btndelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(100, 28);
+            this.btndelete.TabIndex = 20;
+            this.btndelete.Text = "delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click_1);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(33, 401);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdate.TabIndex = 19;
+            this.btnUpdate.Text = "update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(322, 44);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(599, 385);
+            this.dataGridView1.TabIndex = 24;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnview
+            // 
+            this.btnview.Location = new System.Drawing.Point(157, 347);
+            this.btnview.Margin = new System.Windows.Forms.Padding(4);
+            this.btnview.Name = "btnview";
+            this.btnview.Size = new System.Drawing.Size(100, 28);
+            this.btnview.TabIndex = 18;
+            this.btnview.Text = "View Data ";
+            this.btnview.UseVisualStyleBackColor = true;
+            this.btnview.Click += new System.EventHandler(this.btnview_Click_1);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(33, 347);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(100, 28);
+            this.btnsave.TabIndex = 17;
+            this.btnsave.Text = "save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 471);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 17);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 533);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtnamesrch);
+            this.Controls.Add(this.srchbtn);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnview);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.label7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtnamesrch;
+        private System.Windows.Forms.Button srchbtn;
+        private System.Windows.Forms.Button btnnew;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txttax;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtsalery;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnview;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Two_Tier_architecture/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Two_Tier_architecture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=SoftwareArchitectureCourse;Integrated Security=True");
+
+
+        private void load_data()
+        {
+            conn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select * from Register", conn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dataGridView1.DataSource = data;
+            conn.Close();
+        }
+
+
+        private void btnsave_Click_1(object sender, EventArgs e)
+        {
+            if (txtid.Text == "" || txtname.Text == "" || txtsalery.Text == "" || txttax.Text == "" || txtage.Text == "" || comboBox1.Text == "")
+            {
+                MessageBox.Show("please fill the cell first");
+            }
+            else
+            {
+                conn.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("insert into Register(id,name,gender,salery,age,tax)values('" + txtid.Text + "','" + txtname.Text + "','" + comboBox1.Text + "','" + txtsalery.Text + "','" + txtage.Text + "','" + txttax.Text + "')", conn);
+
+                sda.SelectCommand.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("data entered succesfully. . . .");
+                panel1.Enabled = false;
+
+            }
+        }
+
+        private void btnview_Click_1(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            load_data();
+        }
+
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            if (panel1.Enabled == true)
+            {
+
+                if (txtid.Text == "" || txtname.Text == "" || txtsalery.Text == "" || txttax.Text == "" || txtage.Text == "" || comboBox1.Text == "")
+                {
+                    MessageBox.Show("please fill the cell first");
+                }
+                else
+                {
+                    conn.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter("UPDATE Register SET id ='" + txtid.Text + "',name = '" + txtname.Text + "',Gender = '" + comboBox1.Text + "', Salery = '" + txtsalery.Text + "',age = '" + txtage.Text + "',tax = '" + txttax.Text + "' where id ='" + txtid.Text + "'", conn);
+
+                    sda.SelectCommand.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("data updated succesfully. . . .");
+                    load_data();
+                    panel1.Enabled = false;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("please select what you want to update");
+            }
+        }
+
+        private void btndelete_Click_1(object sender, EventArgs e)
+        {
+            if (panel1.Enabled == true)
+            {
+
+                if (txtid.Text == "" || txtname.Text == "" || txtsalery.Text == "" || txttax.Text == "" || txtage.Text == "" || comboBox1.Text == "")
+                {
+                    MessageBox.Show("please select the record");
+                }
+                else
+                {
+                    conn.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter("delete from Register where id ='" + txtid.Text + "'", conn);
+
+                    sda.SelectCommand.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("data deleted succesfully. . . .");
+                    load_data();
+                    panel1.Enabled = false;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("please select the record first");
+            }
+        }
+
+        private void btnnew_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = false;
+            panel1.Enabled = true;
+
+            txtid.Text = "";
+            txtname.Text = "";
+            txtsalery.Text = "";
+            txtage.Text = "";
+            txttax.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void srchbtn_Click_1(object sender, EventArgs e)
+        {
+            if (txtnamesrch.Text == "")
+            {
+                MessageBox.Show("cells are empty");
+            }
+
+            else
+            {
+                conn.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("select * from Register where name LIKE '%" + txtnamesrch.Text + "%'", conn);
+                DataTable data = new DataTable();
+                sda.Fill(data);
+                dataGridView1.DataSource = data;
+                conn.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            panel1.Enabled = true;
+            txtid.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            txtsalery.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtname.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtage.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txttax.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+        }
+    }
+}
